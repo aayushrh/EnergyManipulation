d9650f98d2947034e8673d88174d33e3b0de6f03
MemeFlier <kylehinatahong@gmail.com>
2025-01-27 17:00:36 -0800

    funny border so you can see the style fr

--- style_spin.gd
+++ style_spin.gd
@@ -11,11 +11,15 @@ var tim = 0.0
 func _process(delta):
 	tim += delta
 	global_position = center + dist * Vector2.UP.rotated(float(num+tim)/float(amt) * 2 * PI)
+	#$TextureRect2.scale = Vector2(1.3 + 0.2 * sin(tim*2),1.3 + 0.2 * sin(tim*2))
+	#$TextureRect2.position = Vector2(-20, -20) * $TextureRect2.scale.x
 
 
 
 func changeIcon(t):
 	$TextureRect.texture = t
+	$TextureRect2.texture = t
 
 func changeColor(t):
 	$TextureRect.self_modulate = t
+	$TextureRect2.self_modulate = Color(1.0 - t.r, 1.0 - t.g, 1.0 - t.b)
