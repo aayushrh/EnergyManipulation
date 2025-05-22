11d9bc28529948cc3a3bb4263727302e124fdf9b
aayushrh <73722373+aayushrh@users.noreply.github.com>
2025-05-22 13:30:38 -0700

    Explosions added (maybe)

--- Magic/MagicCasts/Explosion.gd
+++ Magic/MagicCasts/Explosion.gd
@@ -1,50 +1,154 @@
 extends SpellCast
 
+signal done
+
+var direction = Vector2.ZERO
 var castingTimer = 0
+var totalChargeTime = 0
 var chargeTimer = 0
 var shot = false
 var timesShot = 0
 var timer = 0
 var chargeMulti = 1.0
+var speed = 1000.0
+var buttonLetGo = false
+var castingCost = 0
+var slow = true
+var startingLocation = Vector2.ZERO
 
 @onready var ExplosionCast = preload("res://Magic/MagicCasts/ExplosionCast.tscn")
 
+func getMousePos():
+	return get_global_mouse_position()
+
 func setSpell(nspell):
 	spell = nspell
-	castingTimer = spell.getCastingTime()
+	totalChargeTime = spell.getCastingTime()
+	castingTimer = totalChargeTime
 	chargeTimer = spell.getMaxPowerTime()
-	scale = Vector2(1, 1) 
+	#scale = Vector2(0.5, 0.5) * spell.getSize()
+	scale = Vector2(1, 1) * spell.getSize() * _castingAnimationCalc(castingTimer,totalChargeTime)
+	#if(spell.style.size() > 0):
+		#$Sprite2D.texture = spell.style[0].icon
+		#$Sprite2D.scale = Vector2(0.3, 0.3)
+	if(spell.type):
+		$Icon.texture = spell.type.icon
+		$Icon.self_modulate = Color(spell.type.color, 0.5)
+		$Icon.scale = Vector2(0.3, 0.3)
+	for i in range(spell.components.size()):
+		var icon = Sprite2D.new()
+		icon.texture = spell.components[i].icon
+		icon.self_modulate = Color(spell.components[i].color, 0.5)
+		icon.scale = Vector2(0.15, 0.15)
+		icon.position = Vector2(sin(i/(spell.components.size()+0.0) * 2*PI) * 500*0.3, cos(i/(spell.components.size()+0.0) * 2*PI) * 500*0.3)
+		print(icon.position)
+		$Pivot.add_child(icon)
+
+func _ready():
+	if spell != null:
+		for i in spell.components:
+			i.callCastingEffects(self)
+
+func setStartingLoc(pos):
+	startingLocation = pos
+
+func _nameCallout():
+	pass
+	#var magicNameCallout = MagicNameCallout.instantiate()
+	#magicNameCallout.position = Vector2(0, 60)
+	#magicNameCallout._show(spell)
+	#sender.add_child(magicNameCallout)
 
 func _shoot():
 	#print(spell.attributes.getPSpeed())
+	var blastProj = ExplosionCast.instantiate()
+	blastProj.sender = sender
+	blastProj.mult = chargeMulti
+	blastProj.global_position = global_position
+	blastProj._setSpell(spell.create())
+	get_tree().current_scene.add_child(blastProj)
+	if sender.type == 0:
+		get_tree().current_scene.amountShot += 1
+	timer = 0.1 * 1/spell.getASpeed()
+		
 	#direction = Vector2(cos(sender.rotation_degrees * PI/180 - PI/2), sin(sender.rotation_degrees * PI/180 - PI/2))
-	var explosionCast = ExplosionCast.instantiate()
-	explosionCast.mult = chargeMulti
-	explosionCast.global_position = global_position
-	explosionCast._setSpell(spell)
-	get_tree().current_scene.add_child(explosionCast)
-	timer = 0.1
+
+func draw():
+	draw_arc($Icon.position, 256 * 0.3 * chargeMulti, 0, 2*PI, 50, Color.WHITE)
+	draw_circle(Vector2.ZERO, 256, Color.WHITE, false)
 
 func _process(delta):
 	delta *= Global.getTimeScale()
-	if !shot:
-		if (castingTimer >= 0 ):
-			castingTimer -= delta
-		elif chargeTimer >= 0:
-			chargeTimer -= delta
-			if !Input.is_key_pressed(spell.binding):
-				shot = true
-			chargeMulti *= pow(pow(2,delta),1/spell.getMaxPowerTime())
-			scale = Vector2(1, 1) * chargeMulti
+	
+	queue_redraw()
+	$Pivot.rotate(PI/160*delta*120.0)
+	for i in $Pivot.get_children():
+		i.rotate(-PI/160*delta*120.0)
+	if !is_instance_valid(sender):
+		queue_free()
+	if !Global.isPaused() and is_instance_valid(sender):
+		$Sprite2D.global_position = global_position + Vector2(0, -65)
+		if !shot:
+			if startingLocation == Vector2.ZERO:
+				direction = Vector2(cos(sender.rotation_degrees * PI/180 - PI/2), sin(sender.rotation_degrees * PI/180 - PI/2))
+			else:
+				var diff = startingLocation - get_global_mouse_position()
+				var angle = atan2(diff.y, diff.x)
+				direction = Vector2(cos(angle + PI), sin(angle + PI))
+			#direction = Vector2(cos(sender.rotation_degrees * PI/180 - PI/2), sin(sender.rotation_degrees * PI/180 - PI/2))
+			if (castingTimer >= 0 ):
+				castingTimer -= delta
+				scale = Vector2(0.5, 0.5) * spell.getSize() * _castingAnimationCalc(castingTimer,totalChargeTime)
+			elif chargeTimer >= 0:
+				#print(spell.contcost())
+				#print(delta/spell.getMaxPowerTime())
+				if sender.type == 1 or sender.stored_energy > spell.contcost() * delta/spell.getMaxPowerTime():
+					sender.stored_energy -= spell.contcost() * delta/spell.getMaxPowerTime()
+					castingCost += spell.contcost() * delta/spell.getMaxPowerTime()
+					chargeTimer -= delta
+					if buttonLetGo or (spell.binding != null and !Input.is_key_pressed(spell.binding)):
+						shot = true
+						done.emit()
+						_nameCallout()
+						#print(castingCost)
+					scale = Vector2(0.5, 0.5) * spell.getSize()*chargeMulti
+					chargeMulti *= pow(pow(2,delta),1/spell.getMaxPowerTime())
+				else:
+					shot = true
+					done.emit()
+					_nameCallout()
+			else:
+				if buttonLetGo or (spell.binding != null and !Input.is_key_pressed(spell.binding)):
+					shot = true
+					done.emit()
+					_nameCallout()
+			if startingLocation == Vector2.ZERO:
+				global_position = sender.global_position
+			else:
+				var diff = startingLocation - get_global_mouse_position()
+				var angle = atan2(diff.y, diff.x)
+				global_position = startingLocation
 		else:
-			if !Input.is_key_pressed(spell.binding):
-				shot = true
-		global_position = sender.global_position 
-	else:
-		global_position = sender.global_position 
-		timer -= delta
-		if timer < 0:
-			timesShot+=1
-			_shoot()
-			sender.doneCasting(false)
-			queue_free()
+			if startingLocation == Vector2.ZERO:
+				global_position = sender.global_position
+			else:
+				var diff = startingLocation - get_global_mouse_position()
+				var angle = atan2(diff.y, diff.x)
+				global_position = startingLocation
+			timer -= delta
+			if timer <= 0:
+				if timesShot < spell.getAmount():
+					timesShot+=1
+					_shoot()
+					if timesShot >= spell.getAmount():
+						sender.doneCasting()
+						#if(spell.style != null and spell.style.spellName.to_lower() == "horse"):
+							#sender.stored_energy += 0.36 * (spell.initCost() + castingCost)
+						queue_free()
+						spell._notUsing()
+
+func letGo():
+	buttonLetGo = true
+
+func getSpeed():
+	return speed * spell.getPSpeed() * chargeMulti
--- Magic/MagicCasts/ExplosionCast.gd
+++ Magic/MagicCasts/ExplosionCast.gd
@@ -1,17 +1,77 @@
-extends CharacterBody2D
+extends SpellCasted
+class_name Explosion
 
-var spell = null
-var speed = 1000
-var lifetime = 10
-var mult = 1
+var delay_base : float = 0.1
+var radius : float = 100.0
+var sender : CharacterBody2D = null
+var power : float = 0
+var setPower : bool = false
+var mult : float = 1
+var art : Node2D
+var exploded : bool = false
 
-func _setSpell(nspell):
-	scale = Vector2(1, 1) * nspell.attributes.getSize() * mult
+func _setSpell(nspell : Spell) -> void:
 	spell = nspell
+	scale = Vector2.ONE * nspell.getSize() * mult
 
-func _process(delta):
-	delta *= Global.getTimeScale()
-	scale = Vector2(1, 1) * spell.attributes.getSize() * mult
-	lifetime -= delta
-	if(lifetime < 0):
-		queue_free()
+func _setPower(p : float) -> void:
+	power = p
+	setPower = true
+
+func _setRadius(r : float) -> void:
+	radius = r
+
+func getDelay() -> float:
+	# Delay scales inversely with spell speed
+	var speed_factor = max(0.1, spell.getPSpeed())
+	return delay_base / speed_factor
+
+func _ready() -> void:
+	art = $Art
+	if !spell.hasElement():
+		spell.components.append(Global.defaultElement)
+	for i : ComponentSpellCard in spell.components:
+		i.callStartEffects(self)
+
+	# Trigger explosion after a short delay
+	await get_tree().create_timer(getDelay()).timeout
+	explode()
+
+func explode() -> void:
+	if exploded: return
+	exploded = true
+
+	'''# Area check
+	var space_state = get_world_2d().direct_space_state
+	var query = PhysicsShapeQueryParameters2D.new()
+	query.shape = CircleShape2D.new()
+	query.shape.set_radius(radius)
+	query.transform = Transform2D(0, global_position)
+	query.collision_mask = 1 # Adjust as needed
+
+	var results = space_state.intersect_shape(query, 100)
+
+	for result in results:
+		var body = result.collider
+		if is_instance_valid(body) and body.has_method("_hit"):
+			if body != sender and body.type != sender.type:
+				body._hit(self)
+				for i : ComponentSpellCard in spell.components:
+					i.callHitEffects(self, body)
+	'''
+	
+	var bodies = $Area2D.get_overlapping_bodies()
+	for body in bodies:
+		if is_instance_valid(body) and body.has_method("_hit"):
+			if body != sender and body.type != sender.type:
+				body._hit(self)
+				for i : ComponentSpellCard in spell.components:
+					i.callHitEffects(self, body)
+	
+	# Cleanup
+	queue_free()
+
+func damageTaken() -> float:
+	if !setPower:
+		return spell.getPower() * ((mult - 1) / 2.0 + 1) * sender.getIntel()
+	return power
