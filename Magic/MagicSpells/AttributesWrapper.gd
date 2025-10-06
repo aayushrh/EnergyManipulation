4d6dc618e347b7a787166f7e8fe1a40e60e43da3
Kyle Hong <hong.kyle@northeastern.edu>
2025-10-06 17:28:27 -0400

    few helper funcs, a few small fixes
    Watch like everything break anyways despite all these fixes once we actually run it lmao

--- Magic/MagicSpells/AttributesWrapper.gd
+++ Magic/MagicSpells/AttributesWrapper.gd
@@ -15,11 +15,13 @@ func _init(attr:Attributes) -> void:
 		
 		var exp = Expression.new()
 		var nExp = (attr.tags[i].expression.replace("x", "%s"))
-		if exp.parse(nExp % value) != OK:
+		var xCount = nExp.count("%s") - 1
+		if exp.parse(nExp % valueArray(xCount)) != OK:
 			push_error("expression didn't parse right for init rip lol")
 		else:
 			d[e] = {}
 			d[e]["expression"] = nExp
+			d[e]["vCount"] = xCount
 			d[e]["suffix"] = attr.tags[i].suffix
 			d[e]["prefix"] = attr.tags[i].prefix
 			d[e]["default"] = attr.tags[i].default
@@ -32,3 +34,25 @@ func _init(attr:Attributes) -> void:
 	max = attr.max
 	Ltext = attr.Ltext
 	Rtext = attr.Rtext
+
+func calc(i:String) -> float:
+	if(d.has(i)):
+		var exp = Expression.new()
+		if(exp.parse(d[i]["expression"] % valueArray(d[i]["vCount"])) == OK):
+			d[i]["value"] = exp.execute()
+			return d[i]["value"]
+		else:
+			push_error("Something went wrong\nValue: %s\nExpression: %s\ninput: %s" % [value, d[i]["expression"], i])
+	else:
+		push_error("mf dict doesn't have %s as an attributetype" % i)
+	return NAN
+
+func valueArray(i:int) -> Array[float]:
+	var e = []
+	for c in range(i):
+		e.append(value)
+	return e
+
+func calcAll() -> void:
+	for e in d.keys():
+		calc(e)
